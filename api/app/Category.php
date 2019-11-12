2df0572aaf264ac861e963c1513035d710dc7254
Serlin Tamilselvam <t.serlin94@gmail.com>
2019-11-11 19:07:20 -0500

    API: modified productlist

--- api/app/Category.php
+++ api/app/Category.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model {
 	protected $table = 'category';
 	protected $insertElements = ['name'];
+
+	public function getCategoryNameFromId($catId) {
+		$categoryName = Self::selectRaw('category.name')
+		                ->where([['category.cat_id', '=', $catId]])->get()->first();
+		return $categoryName;
+	}
 }
--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\ProductCategoryMapping;
+use App\Category;
 
 class ProductController extends Controller {
 
-	private $productCategoryMap;
+	private $productCategoryMap, $category;
 
-	function __construct(ProductCategoryMapping $productCategoryMap) {
+	function __construct(ProductCategoryMapping $productCategoryMap, Category $category) {
         $this->productCategoryMap = $productCategoryMap;
+        $this->category = $category;
     }
 	
 	public function getProductDetails($id) {
     	try {
     		$response = initResponse();
     		$response['data'] = $this->productCategoryMap->getCategoryProducts($id);
+    		$response['catgeory_title'] = $this->category->getCategoryNameFromId($id);
     		$response = getSuccessResponse($response);
     	} catch(\Exception $ex) {
 	    	$response = getExceptionResponse($ex);
