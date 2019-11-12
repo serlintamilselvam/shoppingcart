51448069eaecdc74512134cdb9c7ade6cf130a48
Serlin Tamilselvam <t.serlin94@gmail.com>
2019-11-11 19:06:04 -0500

    UI: productList page done

--- ui/src/app/views/productlist/productlist.component.ts
+++ ui/src/app/views/productlist/productlist.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs/Observable";
 import * as _ from 'lodash';
 import { ProductService } from './service/product.service';
+
+interface productDetails {
+  prod_id: number,
+  qty: number,
+  rate: number,
+  sku: string,
+  desc: string,
+  name: string,
+  imagepath: string
+}
 
 @Component({
   selector: 'app-productlist',
@@ -11,13 +22,14 @@ import { ProductService } from './service/product.service';
 export class ProductlistComponent implements OnInit {
 
   categoryId: number
+  categoryName: string
+  productInfo$: Observable<productDetails[]>
 
   public getCategoryCollection(id: number) {
     this.api.productCollections(id).subscribe((data: any)=>{
       if (data.response.result.toLowerCase() === 'success') {
-        console.log('api success')
-        // this.categoryInfo$ = _.cloneDeep(data.response.data)
-        // console.log('categoryInfo is ', this.categoryInfo$)
+        this.productInfo$ = _.cloneDeep(data.response.data)
+        this.categoryName = data.response.catgeory_title.name
       } else {
         console.log('please try again after sometime')
       }
